250e5a49d44f18bbf6d4086a977cf46659faf937
paolo flore <paoloflore937@gmail.com>
2024-07-06 14:15:03 +0200

    Aggiungere i file di progetto.

---
+++ esercitazioniFunzioni/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace esercitazioniFunzioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
